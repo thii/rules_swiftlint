f66782ec6933e543f0fd550435c43af3c9729c46
Thi Doãn <t@thi.im>
2023-02-16 19:01:37 +0900

    Remove prefix in archive

--- .github/workflows/workspace_snippet.sh
+++ .github/workflows/workspace_snippet.sh
@@ -5,20 +5,18 @@ set -o errexit -o nounset -o pipefail
 # Set by GH actions, see
 # https://docs.github.com/en/actions/learn-github-actions/environment-variables#default-environment-variables
 TAG=${GITHUB_REF_NAME}
-# The prefix is chosen to match what GitHub generates for source archives
-PREFIX="rules_swiftlint-${TAG:1}"
 ARCHIVE="rules_swiftlint-$TAG.tar.gz"
-git archive --format=tar --prefix=${PREFIX}/ ${TAG} | gzip > $ARCHIVE
+git archive --format=tar ${TAG} | gzip > $ARCHIVE
 SHA=$(shasum -a 256 $ARCHIVE | awk '{print $1}')
 
 cat << EOF
 WORKSPACE snippet:
 \`\`\`starlark
 load("@bazel_tools//tools/build_defs/repo:http.bzl", "http_archive")
+
 http_archive(
     name = "rules_swiftlint",
     sha256 = "${SHA}",
-    strip_prefix = "${PREFIX}",
     url = "https://github.com/thii/rules_swiftlint/releases/download/${TAG}/${ARCHIVE}",
 )
 EOF
